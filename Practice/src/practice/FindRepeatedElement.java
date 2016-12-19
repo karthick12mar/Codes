d7d1f0a077bfc409abbcb5cb6dd879794e057c64
karth <karth@LAPTOP-NESF7U3M>
2016-12-19 11:13:58 -0800

    Is Sorted Array reverse Rotating array n times Finding repeated elements in array

--- Practice/src/practice/FindRepeatedElement.java
+++ Practice/src/practice/FindRepeatedElement.java
@@ -9,28 +9,54 @@ package practice;
  *
  * @author karth
  */
-class FindRepeatedElement
-{
-public static int FindRepeatedElement(int input[],int find)
+class FindRepeatedElement {
+
+    public static int FindRepeatedElement(int input[]) // array size n+1 & containing 1 to n elements  
     {
-        if(input == null)
-        {
-            System.out.println("Empty array");
-            return -1;
-        }
-        if(input.length < 2 & input[0] == find)
-        {
-            return input[0];
-        }
-        else
+        if (input == null || input.length < 2) 
         {
             return -1;
         }
-        int[] count = new count[input.length];
-        for(int i =0;i<input.length;i++)
+        for (int i = 0; i < input.length; i++) 
         {
-            
+            if (input[i] >= input.length)
+            {
+                return -1;
+            }
+            if (input[Math.abs(input[i])] < 0) 
+            {
+                return input[i];
+            } 
+            else
+            {
+                input[Math.abs(input[i])] = -input[Math.abs(input[i])];
+            }
         }
-        return 0;
+        return -1;
+    }
+
+    public static int FindNonRepeatedElement(int input[]) // elements are repeating 2 times expect one element
+    {
+        if (input == null || input.length < 2) 
+        {
+            return -1;
+        }
+        int Xor = input[0];
+        for (int i = 1; i < input.length; i++) 
+        {
+            Xor = Xor ^ input[i];
+        }
+        return Xor;
+        
+    }
+    public static int FindRepeatedElementRandom(int input[])
+    {
+        
+         
+    }
+
+    public static void main(String args[]) {
+        int input[] = {1, 7, 6, 6, 6,1, 1, 7};
+        System.out.println(FindNonRepeatedElement(input));
     }
 }
